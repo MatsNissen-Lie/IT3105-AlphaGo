7e68f024efa49e5780ccb874cdfc9599e6fc289d
Mathias Valhovd <76526229+mathiasvalhovd@users.noreply.github.com>
2024-04-10 19:25:44 +0200

    fixed nim

--- game/nim.py
+++ game/nim.py
@@ -4,58 +4,50 @@ from game.game_interface import GameInterface
 
 
 class Nim(GameInterface):
-    def __init__(self, initial_state, max_take=3):
+    def __init__(self, initial_state, max_take):
         self.state = initial_state  # initial_state should be a list of integers representing piles
         self.max_take = max_take
         self.player_turn = 1
 
-    def get_legal_moves(self, state):
+    def get_legal_moves(self):
         """
         Generate all legal moves within the constraints of the maximum number of objects
         that can be taken in one move.
         """
-        legal_moves = []
-        for pile_index, pile_size in enumerate(state):
-            for objects_to_remove in range(1, min(pile_size, self.max_take) + 1):
-                legal_moves.append((pile_index, objects_to_remove))
-        return legal_moves
+        moves = []
+        for i in range(1, self.max_take+1):
+            moves.append(i)
+        return moves
 
     def is_valid_move(self, move):
         """
         Check if a move is valid given the current state.
         """
-        pile_index, objects_to_remove = move
-        if pile_index < 0 or pile_index >= len(self.state):
-            return False
-        if objects_to_remove < 1 or objects_to_remove > self.max_take:
-            return False
-        if objects_to_remove > self.state[pile_index]:
-            return False
-        return True
+        return ((move >= 1) and (move <= self.max_take) and (self.max_take-move >= 0))
 
-    def make_move(self, state, move):
+
+    def make_move(self, move):
         """
         Apply a move to the state. A move is a tuple (pile_index, objects_to_remove).
         This function returns the new state after the move.
         """
-        pile_index, objects_to_remove = move
-        new_state = state[:]
-        new_state[pile_index] -= objects_to_remove
-        self.player_turn = 3 - self.player_turn  # Switch player
-        return new_state
+        if self.is_valid_move(move):
+            
+            self.player_turn = 3 - self.player_turn  # Switch player
+            self.state -= move 
+            return self
+        else:
+            raise Exception()
 
     def print_piles(self):
         # print the piles nicely
-        pile_string = ""
-        for i, pile in enumerate(self.state):
-            pile_string += f"Pile {i}: {pile} objects\n"
-        print(pile_string)
+        print(self.state)
 
     def is_terminal(self):
         """
         The game is over when all piles are empty.
         """
-        return all(pile == 0 for pile in self.state)
+        return self.state <= 0
 
     def check_win(self):
         if not self.is_terminal():
@@ -69,13 +61,16 @@ class Nim(GameInterface):
         """
         return state
 
+    def get_state(self):
+        return self.state
 
-# Example usage
-initial_state = [3, 4, 5]  # Three piles with 3, 4, and 5 objects respectively
-nim_game = Nim(initial_state)
-print("Legal moves from initial state:", nim_game.get_legal_moves(nim_game.state))
-new_state = nim_game.make_move(
-    nim_game.state, (0, 2)
-)  # Take 2 objects from the first pile
-print("New state after move:", new_state)
-print("Is game over?", nim_game.is_terminal(new_state))
+
+# # Example usage
+# initial_state = [3, 4, 5]  # Three piles with 3, 4, and 5 objects respectively
+# nim_game = Nim(initial_state)
+# print("Legal moves from initial state:", nim_game.get_legal_moves())
+# new_state = nim_game.make_move(
+#     nim_game.state, (0, 2)
+# )  # Take 2 objects from the first pile
+# print("New state after move:", new_state)
+# print("Is game over?", nim_game.is_terminal(new_state))

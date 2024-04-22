eccf7e157c2fb0e96e7d1d7f637c07b811a39a67
Mats Alexander Nissen-Lie <matsani@stud.ntnu.no>
2024-04-22 23:10:11 +0200

    fix: buffer

--- neural_net/actor.py
+++ neural_net/actor.py
@@ -24,10 +24,10 @@ class ReplayBuffer:
         self.replay_batch_size = replay_batch_size
         self.buffer = []
 
-    def add(self, state, action, reward, next_state, done):
+    def add(self, state, target):
         if len(self.buffer) >= self.buffer_size:
             self.buffer.pop(0)
-        self.buffer.append((state, action, reward, next_state, done))
+        self.buffer.append((state, target))
 
     def sample(self):
         batch_size = min(len(self.buffer), self.replay_batch_size)
